369439d347d6240ed09e66a4728145cedd13bff5
eldermiang <61782036+eldermiang@users.noreply.github.com>
2023-06-24 21:13:41 +1000

    Small patch

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
 app.post('/signin', (req, res) => {
   if (req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password) {
-      res.json('Success');
+      res.json(database.users[0]);
   } else {
     res.status(400).json("error logging in");
   }
@@ -51,7 +51,6 @@ app.post('/register', (req, res) => {
     id: 125,
     name: name,
     email: email,
-    password: password,
     entries: 0,
     joined: new Date()
   });
