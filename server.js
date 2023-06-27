b7a7c71fa46afbff0505b4149254769158fd07ea
eldermiang <61782036+eldermiang@users.noreply.github.com>
2023-06-28 00:19:35 +1000

    Cleanup server file and link backend to database

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const cryptoJS = require('crypto-js');
+const knex = require('knex')
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : 5432,
+    user : 'postgres',
+    password : 'password',
+    database : 'smartbrain'
+  }
+});
 
 const PORT = 3000;
 
@@ -9,96 +22,93 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-//For testing only
-const database = {
-  users: [
-    {
-      id: 123,
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: 124,
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date()
-    }
-  ]
-}
-
 app.get('/', (req, res) => {
   res.send(database.users);
 });
 
 app.post('/signin', (req, res) => {
-  if (req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password) {
-      res.json(database.users[0]);
-  } else {
-    res.status(400).json("error logging in");
-  }
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = JSON.stringify(cryptoJS.SHA256(req.body.password).words) === data[0].hash;
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0])
+          })
+          .catch(err => res.status(400).json("Unable to get user"))
+      } else {
+        res.status(400).json("Invalid Credentials");
+      }
+    })
+    .catch(err => {
+      res.status(400).json("Login failed");
+    })
 });
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
+  const hash = JSON.stringify(cryptoJS.SHA256(password).words);
 
-  database.users.push({
-    id: 125,
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date()
-  });
-  res.json(database.users.at(-1));
+  db.transaction(trx => {
+    trx.insert({
+      hash: hash,
+      email: email
+    })
+    .into('login')
+    .returning('email')
+    .then(loginEmail => {
+      trx('users')
+        .returning('*')
+        .insert({
+          email: loginEmail[0].email,
+          name: name,
+          joined: new Date()
+        })
+        .then(user => {
+          res.json(user[0]);
+        })
+    })
+    .then(trx.commit)
+    .catch(trx.rollback)
+  })
+    .catch(err => res.status(400).json('Registration failed'));
+
 });
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
 
-  database.users.forEach(user => {
-    if (user.id == id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    return res.status(400).json("User not found");
-  }
+  db.select().from('users').where({
+    id: id
+  })
+    .then(user => {
+      if (user.length) {
+        res.json(user);
+      } else {
+        res.status(400).json("User not found");
+      }
+      
+    })
+    .catch(err => {
+      res.status(400).json("Error getting user");
+    })
 });
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-
-  database.users.forEach(user => {
-    if (user.id == id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(400).json("User not found");
-  }
+  db('users').where({
+    id: id
+  })
+  .increment('entries', 1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0].entries);
+  })
+  .catch(err => res.status(400).json("Unable to get entries"));
 });
 
 app.listen(PORT, () => {
   console.log('app is running on port ' + PORT);
 });
-
-/* 
-
-/ --> res = this is working
-/signin --> POST = success/fail
-/register --> POST = user
-/profile/:userId --> GET = user
-/image --> PUT --> user (imageCount)
-
-
-*/
