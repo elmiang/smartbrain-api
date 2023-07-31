7abb93eef1f00b0f5a3c2cb3356380478fc2f8bc
elmiang <eldermiang@gmail.com>
2023-07-31 14:14:23 +1000

    Add and restructure route for rankings

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,13 @@
-const getRankings = (req, res, db) => {
-  db.select('name', 'entries').from('users').orderBy('entries', 'desc')
+const getRankingsEntries = (req, res, db) => {
+  db.select('name', 'entries', 'faces').from('users').orderBy('entries', 'desc')
+    .then(data => res.json(data))
+    .catch(err => {
+      res.status(400).json("Failed to get rankings");
+    })
+}
+
+const getRankingsFaces = (req, res, db) => {
+  db.select('name', 'entries', 'faces').from('users').orderBy('faces', 'desc')
     .then(data => res.json(data))
     .catch(err => {
       res.status(400).json("Failed to get rankings");
@@ -7,5 +15,6 @@ const getRankings = (req, res, db) => {
 }
 
 module.exports = {
-  getRankings
+  getRankingsEntries,
+  getRankingsFaces
 }
